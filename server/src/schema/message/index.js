d625a4e4feb799f344f0e281232f1fa09f1bc1d2
Sébastien Lampazona <lampazona.sebastien@gmail.com>
2021-10-03 19:18:34 +0200

    Ajout de la base64 pour les images dans l'api

--- server/src/schema/message/index.js
+++ server/src/schema/message/index.js
@@ -1,4 +1,4 @@
-const MESSAGES = []
+const MESSAGES = [];
 
 export const MessageResolvers = {
   Query: {
@@ -11,14 +11,41 @@ export const MessageResolvers = {
   },
   Mutation: {
     post: (root, args, context) => {
-      const {image, message} = args;
-      const messageObj = {
-        image: image,
-        message,
-        created_at: new Date(),
-      };
-      MESSAGES.push(messageObj);
-      return messageObj;
+      return new Promise(async (resolve, reject) => {
+        try {
+          const { image, message } = args;
+
+          const {
+            createReadStream,
+            filename,
+            mimetype,
+            encoding,
+          } = await image;
+
+          const stream = createReadStream();
+
+          var data = `data:${mimetype};base64,`;
+          stream.on('readable', function () {
+            data += stream?.read()?.toString('base64') || '';
+          });
+          stream.on('end', function () {
+            const messageObj = {
+              message,
+              image: {
+                filename,
+                mimetype,
+                encoding,
+                base64: data
+              },
+              created_at: new Date(),
+            };
+            MESSAGES.push(messageObj);
+            resolve(messageObj);
+          });
+        } catch (e) {
+          reject(e);
+        }
+      });
     },
-  }
+  },
 };
